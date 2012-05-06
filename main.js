0cf0d84c13774b24fb6ac47ce111ffe12b2904d9
Jesse Nave <jesse.nave1@gmail.com>
2012-05-06 11:26:41 -0400

    fixed main.js

--- main.js
+++ main.js
@@ -5,9 +5,7 @@
     Author: Jesse Nave
 */
 
-//$(document).ready(function(){
-    
- /*   
+/*
     function showValue(newValue){
         document.getElementById("range").innerHTML = newValue;
     }
@@ -130,41 +128,59 @@
 */
 
 //Wait until the DOM is ready.
-window.addEventListener("DOMContentLoaded", function () {
-   /* 
-        function drawImage(){
-        var can = document.getElementById("canvas1");
-        
-        if(can && can.getContext){
-            var drw = can.getContext("2d");
-            if(drw){
-                var img1 = document.getElementById("hero");
-                drw.drawImage(img1, 0,0);
-            }
-        }
-    }
+$(document).ready(function () {
+    
+//        function drawImage(){
+//        var can = $("#canvas1");
+//
+//        if(can && can.getContext){
+//            var drw = can.getContext("2d");
+//            if(drw){
+//                var img1 = $("#hero");
+//                drw.drawImage(img1, 0,0);
+//            }
+//        }
+//    }
 
-   */
+//get data in three different formats form external files
+
+/*------------------------------------------------------------------------*/
+
+$("#buttonJson").click(function(){
+    
+    });
+
+$("#buttonXML").click(function(){
+    
+    });
+
+$("#buttonCSV").click(function(){
+    
+    });
+
+/*------------------------------------------------------------------------*/
+
+    
     //getElementById Function
-    function $(x){
+    function getId(x){
     var theElement = document.getElementById(x);
     return theElement;
     }
     
     //Create select field element and populate with options
     function makeCats(){
-        var formTag = document.getElementsByTagName("form"),//formTag is an array of all the form tags(because we used getElementsByTagName (plural))
-            selectLi = $("select"),
-            makeSelect = document.createElement('select');
-            makeSelect.setAttribute("id", "groups");
+        var formTag = $("form"),//formTag is an array of all the form tags. (because we used getElementsByTagName (plural))
+            selectLi = $("#select"),
+            makeSelect = $.('<select></select>');       // $('<select></select>')
+            makeSelect.attr("id", "groups");    // makeSelect.attr("id", "groups");
         for(var i = 0, j = contactGroups.length; i<j; i++){
-            var makeOption = document.createElement("option");
+            var makeOption = $.create("option");
             var optText = contactGroups[i];
-            makeOption.setAttribute("value", optText);
-            makeOption.innerHTML = optText;
-            makeSelect.appendChild(makeOption);
+            makeOption.attr("value", optText);
+            makeOption.html = optText;  // .html
+            makeSelect.append(makeOption);    // .append
         }
-        selectLi.appendChild(makeSelect);
+        selectLi.append(makeSelect);
     }
     
     //find value of selected radio button
@@ -179,18 +195,18 @@ window.addEventListener("DOMContentLoaded", function () {
 */
     
     function getCheckboxValue(){
-        if($("fav").checked){
-            comicFaveValue = $("fav").value;
+        if($("#fav").checked){   // $("#fav").checked
+            comicFaveValue = $("#fav").value;
         }else{
             comicFaveValue = "No";
         }
-        if($("fave1").checked){
-            pencilFaveValue = $("fave1").value;
+        if($("#fave1").checked){
+            pencilFaveValue = $("#fave1").value;
         }else{
             pencilFaveValue = "No";
         }
-        if($("fave2").checked){
-            writerFaveValue = $("fave2").value;
+        if($("#fave2").checked){
+            writerFaveValue = $("#fave2").value;
         }else{
             writerFaveValue = "No";
         }
@@ -199,18 +215,18 @@ window.addEventListener("DOMContentLoaded", function () {
     function toggleControls(n){
         switch(n){
             case "on":
-                $('contactForm').style.display = "none";
-                $('clear').style.display = "inline";
-                $('displayLink').style.display = "none";
-                $('addNew').style.display = "inline";
+                $('#contactForm').style.display = "none";
+                $('#clear').style.display = "inline";
+                $('#displayLink').style.display = "none";
+                $('#addNew').style.display = "inline";
                 break;
             
             case "off":
-                $('contactForm').style.display = "block";
-                $('clear').style.display = "inline";
-                $('displayLink').style.display = "inline";
-                $('addNew').style.display = "none";
-                $('items').style.display = "none";
+                $('#contactForm').style.display = "block";
+                $('#clear').style.display = "inline";
+                $('#displayLink').style.display = "inline";
+                $('#addNew').style.display = "none";
+                $('#items').style.display = "none";
                 break;
             
             default:
@@ -230,27 +246,27 @@ window.addEventListener("DOMContentLoaded", function () {
             id = key;
         }
         //Gather up all our form field values and store them in an object.
-        //Object porperties contain array with the form label and input value.
+        //Object properties contain array with the form label and input value.
         getCheckboxValue();
         var item = {};
-        item.title          = ["Title: ", $('title').value];
-        item.volume         = ["Volume: ", $('volume').value];
-        item.comicNum       = ["ComicNum: ", $('comicNum').value];
-        item.pubDate        = ["PubDate: ", $('pubDate').value];
-        item.publisher      = ["Publisher: ", $('groups').value];
-        item.count          = ["Ammount: ", $('slide').value];
+        item.title          = ["Title: ", $('#title').value]; //
+        item.volume         = ["Volume: ", $('#volume').value];
+        item.comicNum       = ["ComicNum: ", $('#comicNum').value];
+        item.pubDate        = ["PubDate: ", $('#pubDate').value];
+        item.publisher      = ["Publisher: ", $('#groups').value];
+        item.count          = ["Ammount: ", $('#slide').value];
         
         item.comicFave      = ["Is a Favorite: ", comicFaveValue];
-        item.notes          = ["Notes: ", $('notes').value];
-        item.penciler       = ["Penciler: ", $('penciler').value];
+        item.notes          = ["Notes: ", $('#notes').value];
+        item.penciler       = ["Penciler: ", $('#penciler').value];
         
         item.pencilFave     = ["Favorite Artist:", pencilFaveValue];
-        item.writer         = ["Writer: ", $('writer').value];
+        item.writer         = ["Writer: ", $('#writer').value];
         
         item.writerFave     = ["Favorite Writer:", writerFaveValue];
-        item.oPrice         = ["OriginalPrice: ", $('oPrice').value];
-        item.cValue         = ["CurrentValue: ", $('cValue').value];
-        //save data into local storage: using stringivy to convert object into a string
+        item.oPrice         = ["OriginalPrice: ", $('#oPrice').value];
+        item.cValue         = ["CurrentValue: ", $('#cValue').value];
+        //save data into local storage: using stringify to convert object into a string
         localStorage.setItem(id, JSON.stringify(item));
         alert("Comic Saved!");
     }
@@ -264,29 +280,29 @@ window.addEventListener("DOMContentLoaded", function () {
             alert("No comics saved yet, so default data was added.");
         }
         //write data from local storage to browser
-        var makeDiv = document.createElement('div');
-        makeDiv.setAttribute("id", "items");
-        var makeList = document.createElement('ul');
-        makeDiv.appendChild(makeList);
-        document.body.appendChild(makeDiv);
-        $('items').style.display = "block";
+        var makeDiv = $.create('div');
+        makeDiv.attr("id", "items");
+        var makeList = $.create('ul');
+        makeDiv.append(makeList);
+        $.body.append(makeDiv);
+        $('#items').style.display = "block";
         for(var i = 0, len=localStorage.length; i<len; i++){
-            var makeli = document.createElement("li");
-            var linksLi = document.createElement("li");
-            makeList.appendChild(makeli);
+            var makeli = $.create("li");
+            var linksLi = $.create("li");
+            makeList.append(makeli);
             var key = localStorage.key(i);
             var value = localStorage.getItem(key);
             //convert the string from local storage value back to an object by using JSON.parse
             var obj = JSON.parse(value);
-            var makeSublist = document.createElement('ul');
-            makeli.appendChild(makeSublist);
+            var makeSublist = $.create('ul');
+            makeli.append(makeSublist);
             //getImage(obj.groups[1], makeSublist);
             for(var n in obj){
-                var makeSubli = document.createElement("li");
-                makeSublist.appendChild(makeSubli);
+                var makeSubli = $.create("li");
+                makeSublist.append(makeSubli);
                 var optSubText = obj[n][0] + " " + obj[n] [1];
-                makeSubli.innerHTML = optSubText;
-                makeSublist.appendChild(linksLi);
+                makeSubli.html = optSubText;
+                makeSublist.append(linksLi);
             }
             makeItemLinks(localStorage.key(i), linksLi);//Create our edit and delete buttons/link for each item in local storage.
         }
@@ -296,7 +312,7 @@ window.addEventListener("DOMContentLoaded", function () {
     //Get the image for the correct category
     
     //this code breaks my app
-    //The video doesn't show where he gets the obj "group".  I have tried every name that I used to describe my
+    //The video doesn't show where he gets the obj "group".  I have tried ever name that I used to describe my
     //dropdown list, but none of them work.
     
    /* function getImage(catName, makeSublist){
@@ -336,26 +352,26 @@ window.addEventListener("DOMContentLoaded", function () {
     function makeItemLinks(key, linksLi){
         
         //add edit single item link.
-        var editLink = document.createElement('a');
+        var editLink = $.create('a');
         editLink.href = "#";
         editLink.key = key;
         var editText = "Edit Comic";
         editLink.addEventListener("click", editItem);
         editLink.innerHTML = editText;
-        linksLi.appendChild(editLink);
+        linksLi.append(editLink);
         
         //add line break
-        var breakTag = document.createElement('br');
-        linksLi.appendChild(breakTag);
+        var breakTag = $.create('br');
+        linksLi.append(breakTag);
         
         //add delete single item link
-        var deleteLink = document.createElement('a');
+        var deleteLink = $.create('a');
         deleteLink.href = "#";
         deleteLink.key = key;
         var deleteText = "Delete Comic";
         deleteLink.addEventListener("click", deleteItem);
-        deleteLink.innerHTML = deleteText;
-        linksLi.appendChild(deleteLink);
+        deleteLink.html = deleteText;
+        linksLi.append(deleteLink);
          
     }
     
@@ -368,38 +384,38 @@ window.addEventListener("DOMContentLoaded", function () {
         toggleControls("off");
         
         //populate form fields with current local storage values
-        $('title').value = item.title[1];
-        $('volume').value = item.volume[1];
-        $('comicNum').value = item.comicNum[1];
-        $('pubDate').value = item.pubDate[1];
-        $('groups').value = item.publisher[1];
-        $('count').value = item.count[1];
+        $('#title').value = item.title[1];
+        $('#volume').value = item.volume[1];
+        $('#comicNum').value = item.comicNum[1];
+        $('#pubDate').value = item.pubDate[1];
+        $('#groups').value = item.publisher[1];
+        $('#count').value = item.count[1];
         
-        $('comicFave').value = item.comicFave[1];
-        $('notes').value = item.notes[1];
-        $('penciler').value = item.penciler[1];
+        $('#comicFave').value = item.comicFave[1];
+        $('#notes').value = item.notes[1];
+        $('#penciler').value = item.penciler[1];
         
-        $('pencilFave').value = item.pencilFave[1];
-        $('writer').value = item.group[1];
+        $('#pencilFave').value = item.pencilFave[1];
+        $('#writer').value = item.group[1];
         
-        $('writerFave').value = item.writerFave[1];
-        $('oPrice').value = item.oPrice[1];
-        $('cValue').value = item.cValue[1];
+        $('#writerFave').value = item.writerFave[1];
+        $('#oPrice').value = item.oPrice[1];
+        $('#cValue').value = item.cValue[1];
         
         if(item.comicFave[1] == "Yes"){
-            $('comicFave').setAttribute("checked", "checked");
+            $('#comicFave').attr("checked", "checked");
         }
         if(item.pencilFave[1] == "Yes"){
-            $('pencilFave').setAttribute("checked", "checked");
+            $('#pencilFave').attr("checked", "checked");
         }
         if(item.writerFave[1] == "Yes"){
-            $('writerFave').setAttribute("checked", "checked");
+            $('#writerFave').attr("checked", "checked");
         }
         //Remove listener from the save button
         save.removeEventListener("click", storeData);
         //change submit button value to say "Edit Button"
-        $('submita').value = "Edit Comic";
-        var editSubmit = $('submita');
+        $('#submita').value = "Edit Comic";
+        var editSubmit = $('#submita');
         //save the key value from this function as a property of the editSubmit event
         //so we can use that value to save the data that we edited
         editSubmit.addEventListener("click", validate);
@@ -432,14 +448,14 @@ window.addEventListener("DOMContentLoaded", function () {
         function validate(e){
         
         //define elements that we want to check
-        var getTitle = $('title');
-        var getVolume = $('volume');
-        var getComicNum = $('comicNum');
-        var getPubDate = $('pubDate');
-        var getPublisher = ('publisher');
+        var getTitle = $('#title');
+        var getVolume = $('#volume');
+        var getComicNum = $('#comicNum');
+        var getPubDate = $('#pubDate');
+        var getPublisher = $('#publisher');
         
         //reset the error messages
-        errMsg.innerHTML = "";
+        errMsg.html = "";
         getTitle.style.border = "1px solid black";
         getVolume.style.border = "1px solid black";
         getComicNum.style.border = "1px solid black";
@@ -477,9 +493,9 @@ window.addEventListener("DOMContentLoaded", function () {
         //If there are any errors, display them on the screen.
         if(messageAry.length >= 1){
             for(var i = 0, j = messageAry.length; i < j; i++){
-                var txt = document.createElement('li');
-                txt.innerHTML = messageAry[i];
-                errMsg.appendChild(txt);
+                var txt = $.create('li');
+                txt.html = messageAry[i];
+                errMsg.append(txt);
             }
             
         }
@@ -490,7 +506,7 @@ window.addEventListener("DOMContentLoaded", function () {
     //Variable defaults
     var contactGroups = ["---Choose A Publisher---", "MARVEL", "DC", "Image", "Dark_Horse", "Full_Bleed", "Other"],
     comicFaveValue = "No",
-    errMsg = $('errors');
+    errMsg = $('#errors');
     
     ;
     makeCats();
@@ -498,14 +514,14 @@ window.addEventListener("DOMContentLoaded", function () {
     //getData();
     
     //Set Link & Submit Click Events
-    var displayLink = $('displayLink');
+    var displayLink = $('#displayLink');
     displayLink.addEventListener("click", getData);
-    var clearLink = $("clear");
+    var clearLink = $("#clear");
     clearLink.addEventListener("click", clearLocal);
-    var save = $("submita");
+    var save = $("#submita");
     save.addEventListener("click", storeData);
     
 
 });
 
-//});
+
